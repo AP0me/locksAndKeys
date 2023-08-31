41b5078aa88883329d62fcd67c17ebfb3a1ab689
Anar <121792402+AP0me@users.noreply.github.com>
2023-08-31 11:18:43 +0400

    Add files via upload

--- locksAndkeys.py
+++ locksAndkeys.py
@@ -13,33 +13,61 @@ random.shuffle(allLocks)
 
 for i in range(len(allLocks)):
   allLocks[i][0] = allLocks[i][0].replace("K", "L")
-myPosition = random.randint(0, len(allLocks) - 1)
+
+while 1:
+  play1Position = random.randint(0, len(allLocks) - 1)
+  play2Position = random.randint(0, len(allLocks) - 1)
+  if play1Position != play2Position:
+    break
 
 measure = []
 for i in range(len(allLocks)):
   measure.append(" "+str(i)+"  ")
 
 youwin = False
+playerPosition=play1Position
 while not youwin:
-  print(measure)
-  print(allLocks)
-  print(allKeys)
+  if playerPosition==play1Position:
+    playerPosition=play2Position
+  elif playerPosition==play2Position:
+    playerPosition=play1Position
+  else:
+    print("WTF")
+
+
+  print(measure , ": Indexes")
+  print(allLocks, ": Locks")
+  print(allKeys , ": Keys ")
   
-  print("You are behind lock", allLocks[myPosition][0], "at position", myPosition, "open your lock.")
-  inpLockIndex = int(input("Choose a lock index to open: "))
-
-  hasCorrectKey = False
-  for i in range(len(allKeys[myPosition])):
-    if allLocks[inpLockIndex][0][1] == allKeys[myPosition][i][1]:
-      if myPosition == inpLockIndex:
-        youwin = True
+  print("You are locked at position", playerPosition, "escape.")
+  quits=0
+  while 1:
+    inpLockIndex = input("Trade your key with another key. Choose by Entering its index: \n")
+    if inpLockIndex=="quit":
+      quits=1
+    elif inpLockIndex.isdigit():
+      inpLockIndex = int(inpLockIndex)
+      if -1<inpLockIndex<len(allLocks):
+        break
+      else:
+        print(inpLockIndex, "is not a valid index.")
+    else:
+      print(inpLockIndex, "is not an index.")
+  if quits:
+    break
+  
+  if allLocks[inpLockIndex][0][1] == allKeys[playerPosition][0][1]:
+    if playerPosition == inpLockIndex:
+      if playerPosition==play1Position:
+        print("\n                     !!!!! Player1 Wins !!!!!")
+        break
+      elif playerPosition==play2Position:
+        print("\n                     !!!!! Player2 Wins !!!!!")
+        break
+    else:
       gainedKey  = allKeys[inpLockIndex]
-      allKeys[inpLockIndex] = allKeys[myPosition]
-      allKeys[myPosition] = gainedKey
-
-      hasCorrectKey = True
-  if hasCorrectKey == False:
-    print("\n                     !!!!!You don't have correct Key!!!!!\n", allLocks[inpLockIndex][0], allKeys[myPosition][i])
-
-print("You Win")
+      allKeys[inpLockIndex] = allKeys[playerPosition]
+      allKeys[playerPosition] = gainedKey
+  else:
+    print("\n                     !!!!!You don't have correct Key!!!!!\n", allLocks[inpLockIndex][0], allKeys[playerPosition][0])
 
