9e6897b302adc01f6ccd2f7c7bb219e79679155c
Anar <121792402+AP0me@users.noreply.github.com>
2023-08-31 17:11:49 +0400

    Add files via upload

--- locksAndkeys.py
+++ locksAndkeys.py
@@ -1,6 +1,25 @@
 #this game works
 import random
 
+def checkInput(inpLockIndex, quits):
+  if inpLockIndex=="quit":
+    quits=1
+    return [quits, None]
+  elif inpLockIndex.isdigit():
+    inpLockIndex = int(inpLockIndex)
+    if -1<inpLockIndex<len(allLocks):
+      if allLocks[inpLockIndex][0][1] == allKeys[playerPosition][0][1]:
+        return [quits, inpLockIndex, "breaks"]
+      else:
+        print("Key", allKeys[playerPosition][0], "doesn't fit", allLocks[inpLockIndex][0])
+        return [quits, inpLockIndex, None]
+    else:
+      print(inpLockIndex, "is not a valid index.")
+      return [quits, None, None]
+  else:
+    print(inpLockIndex, "is not an index.")
+    return [quits, None, None]
+
 allKeys = []
 for i in range(10):
   rKeyNum = random.randint(1, 9)
@@ -24,50 +43,55 @@ measure = []
 for i in range(len(allLocks)):
   measure.append(" "+str(i)+"  ")
 
-youwin = False
-playerPosition=play1Position
-while not youwin:
+youwin=0
+quits=0
+playerPosition=play2Position
+while not youwin and not quits:
   if playerPosition==play1Position:
     playerPosition=play2Position
+    input("Press Enter to: Switch to player2")
   elif playerPosition==play2Position:
     playerPosition=play1Position
+    input("Press Enter to: Switch to player1")
   else:
     print("WTF")
-
+  
 
   print(measure , ": Indexes")
   print(allLocks, ": Locks")
   print(allKeys , ": Keys ")
   
-  print("You are locked at position", playerPosition, "escape.")
-  quits=0
-  while 1:
-    inpLockIndex = input("Trade your key with another key. Choose by Entering its index: \n")
-    if inpLockIndex=="quit":
-      quits=1
-    elif inpLockIndex.isdigit():
-      inpLockIndex = int(inpLockIndex)
-      if -1<inpLockIndex<len(allLocks):
-        break
-      else:
-        print(inpLockIndex, "is not a valid index.")
+  print("You are locked behind position", playerPosition, "Escape.")
+  goodInput=0
+  while not goodInput:
+    inpLockIndex = input("Trade your key with another key. Choose by Entering its index or 'quit' to quit the game: \n")
+    [quits, inpLockIndex, breaks] = checkInput(inpLockIndex, quits)
+    if breaks!=None:
+      goodInput=1
     else:
-      print(inpLockIndex, "is not an index.")
+      if playerPosition==play1Position:
+        playerPosition=play2Position
+      elif playerPosition==play2Position:
+        playerPosition=play1Position
+      else:
+        print("WTF")
+      break
+
+    if quits:
+      break
+    if allLocks[inpLockIndex][0][1] == allKeys[playerPosition][0][1]:
+      if playerPosition == inpLockIndex:
+        if playerPosition==play1Position:
+          print("\n                     !!!!! Player1 Wins !!!!!")
+          youwin=1
+        elif playerPosition==play2Position:
+          print("\n                     !!!!! Player2 Wins !!!!!")
+          youwin=1
+      else:
+        gainedKey  = allKeys[inpLockIndex]
+        allKeys[inpLockIndex] = allKeys[playerPosition]
+        allKeys[playerPosition] = gainedKey
+      break
+
   if quits:
     break
-  
-  if allLocks[inpLockIndex][0][1] == allKeys[playerPosition][0][1]:
-    if playerPosition == inpLockIndex:
-      if playerPosition==play1Position:
-        print("\n                     !!!!! Player1 Wins !!!!!")
-        break
-      elif playerPosition==play2Position:
-        print("\n                     !!!!! Player2 Wins !!!!!")
-        break
-    else:
-      gainedKey  = allKeys[inpLockIndex]
-      allKeys[inpLockIndex] = allKeys[playerPosition]
-      allKeys[playerPosition] = gainedKey
-  else:
-    print("\n                     !!!!!You don't have correct Key!!!!!\n", allLocks[inpLockIndex][0], allKeys[playerPosition][0])
-
